29201d671fd6bd33f38411dee4b6d8e677b5ad9f
khairil4565 <151598437+khairil4565@users.noreply.github.com>
2025-08-15 12:55:22 +0800

    Update novelfull.js

--- src/novelfull.js
+++ src/novelfull.js
@@ -1,1 +1,26 @@
-test
+{
+  "id": "novelfull",
+  "name": "NovelFull",
+  "version": "1.0.0",
+  "base_url": "https://novelfull.net",
+  "endpoints": {
+    "latest_novels": "/latest-release-novel",
+    "hot_novels": "/hot-novel",
+    "search": "/search?keyword={query}",
+    "novel_detail": "/{novel-slug}.html",
+    "chapter_content": "/{novel-slug}/{chapter-slug}.html"
+  },
+  "selectors": {
+    "novel_list": ".list.list-truyen .row",
+    "novel_title": ".truyen-title a",
+    "novel_url": ".truyen-title a",
+    "novel_cover": ".book img",
+    "chapter_list": ".list-chapter li a",
+    "chapter_title": ".chapter-title",
+    "chapter_content": "#chapter-content"
+  },
+  "patterns": {
+    "novel_slug_regex": "https://novelfull.net/(.+?).html",
+    "chapter_slug_regex": "https://novelfull.net/.+?/(.+?).html"
+  }
+}
